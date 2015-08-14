f66d44ce4bbedc5359ad2908475fe4c80baadac8
Felipe Nuila <felnuila@gmail.com>
2015-08-14 14:53:01 -0400

    Refactors test headers for each request

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -59,7 +60,13 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/v/3-0/docs
   config.infer_spec_type_from_file_location!
 
-  # Including to test requests json helpders
+  # Including to test requests
   config.include Request::JsonHelpers, :type => :controller
-  
+  config.include Request::HeadersHelpers, :type => :controller
+
+  # Including accept headers
+  config.before(:each, type: :controller) do
+    include_default_accept_headers
+  end
+
 end
